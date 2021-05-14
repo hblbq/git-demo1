8dc895425e440454fed42aa536557f7e0d3a8fc0
lbq <1445719095@qq.com>
2021-05-14 10:11:23 +0800

    second commit

--- src/main/java/com/hbsi/git/GitTest.java
+++ src/main/java/com/hbsi/git/GitTest.java
@@ -8,5 +8,6 @@ package com.hbsi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git Test");
+        System.out.println("git Test2222");
     }
 }

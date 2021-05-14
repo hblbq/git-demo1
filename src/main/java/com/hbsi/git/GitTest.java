6368ad35e10064b0aa2882ae91f7904f296c9537
lbq <1445719095@qq.com>
2021-05-14 11:10:44 +0800

    hot-fix second commit

--- src/main/java/com/hbsi/git/GitTest.java
+++ src/main/java/com/hbsi/git/GitTest.java
@@ -11,5 +11,7 @@ public class GitTest {
         System.out.println("git Test2222");
         System.out.println("git Test3333");
         System.out.println("git Test4444");
+        System.out.println("git Test5555");
+        
     }
 }
